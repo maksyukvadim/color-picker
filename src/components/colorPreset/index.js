a9d4ba500937fb0b9de370e8aa290c4952dee583
Maksyuk Vadim <maksyukvadim@gmail.com>
2018-04-17 20:28:18 +0300

    add utils for testing

--- src/components/colorPreset/index.js
+++ src/components/colorPreset/index.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './color-presets.css';
 import dropDown from '../../icons/arrowDropdown.svg';
 import { COLORS_LIST } from '../../constants';
 
-class ColorPreset extends Component {
-    render() {
-        const { onOpen, open } = this.props;
-        return (
-            <div className="ColorPreset" onClick={onOpen}>
-                <img src={dropDown} alt=""/>
-                {open && <div className='ColorPreset__dropdown' >
-                    {COLORS_LIST.map( item =>
-                        <div className='ColorPreset__dropdown-item' key={item}>
-                            <div className='dropdown-item__text'>{item.toUpperCase()}</div>
-                            <div className='dropdown-item__block-color' style={{background: item}} />
-                        </div> )}
-                </div>}
-            </div>
-        );
-    }
-}
+const ColorPreset = ({onOpen, open}) => (
+    <div className="ColorPreset" onClick={onOpen}>
+        <img src={dropDown} alt=""/>
+        {open && <div className='ColorPreset__dropdown' >
+            {COLORS_LIST.map( color =>
+                <div className='ColorPreset__dropdown-item' key={color}>
+                    <div className='dropdown-item__text'>{color.toUpperCase()}</div>
+                    <div className='dropdown-item__block-color' style={{background: color}} />
+                </div> )}
+        </div>}
+    </div>
+);
 
 export default ColorPreset;
